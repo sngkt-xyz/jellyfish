c2a8123d3db9038d43111b64511dcd79bde57538
Saefulloh Maslul <maslulsaefulloh@gmail.com>
2022-05-02 23:30:09 +0700

    refactor: improve wrap library

--- internal/libraries/wrap_library.go
+++ internal/libraries/wrap_library.go
@@ -33,11 +33,10 @@ func formatAPIResponse(statusCode int, headers http.Header, responseData string)
 	// 	IsBase64Encoded: false,
 	// }, nil
 	return &events.APIGatewayProxyResponse{
-		StatusCode:        200,
-		Headers:           map[string]string{"Content-Type": "text/plain"},
-		MultiValueHeaders: http.Header{"Set-Cookie": {"Ding", "Ping"}},
-		Body:              "Hello, World!",
-		IsBase64Encoded:   false,
+		StatusCode:      statusCode,
+		Headers:         map[string]string{"Content-Type": "text/plain"},
+		Body:            responseData,
+		IsBase64Encoded: false,
 	}, nil
 }
 
